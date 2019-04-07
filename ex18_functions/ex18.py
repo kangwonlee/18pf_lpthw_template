4c58470f4dee09519e252d460ae3ab5151503464
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-04-07 22:12:42 +0900

    text comment

--- ex18_functions/ex18.py
+++ ex18_functions/ex18.py
@@ -8,3 +8,11 @@
 # print 'abc', abc, 'def' -> print('abc %s def' % (abc))
 # print 'abc', -> print('abc', end=" ")
 # raw_input('abc') -> input('abc')
+
+# argv를 쓴 스크립트와 비슷한 함수
+
+# 좋아요. 사실 *args는 필요가 없습니다. 그냥 이렇게 하죠.
+
+# 이 함수는 실행인자를 하나만 받습니다
+
+# 이 함수는 실행인자를 하나도 받지 않습니다
