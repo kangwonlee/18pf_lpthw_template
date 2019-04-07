62304caa6eae1efcd812708590745076e3d9601c
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-04-07 22:05:30 +0900

    text comments

--- ex02_comment/ex02.py
+++ ex02_comment/ex02.py
@@ -4,3 +4,9 @@
 # 각 행 주석 입력 후 commit / Enter comment for each line and commit
 # 각자 Study drills 시도 후 필요시 commit / Try Study Drills and commit if necessary
 # print 'abc' -> print('abc')
+
+# 주석, 나중에 프로그램을 읽을 수 있게 도와줍니다.
+# 파이썬에서 # 뒤에 쓰인 것은 무엇이든 무시됩니다.
+
+# 그리고 주석 뒤는 무시됩니다.
+# print("실행되지않습니다.")
